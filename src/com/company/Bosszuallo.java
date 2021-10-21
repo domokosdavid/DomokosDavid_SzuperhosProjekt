3b66f699d6ca3763fa04f247b5767f04d28f0615
domokosdavid <77434649+domokosdavid@users.noreply.github.com>
2021-10-21 12:25:05 +0200

    Getterek, setterek létrehozása, to string metódus

--- src/com/company/Bosszuallo.java
+++ src/com/company/Bosszuallo.java
@@ -3,12 +3,15 @@ package com.company;
 public abstract class Bosszuallo implements Szuperhos{
 
     private double szuperero;
+
     private boolean vanEGyengesege;
 
     public Bosszuallo (double szuperero , boolean vanEGyengesege ) {
-        this . szuperero = szuperero ;
-        this . vanEGyengesege = vanEGyengesege ;
-        }
+        this.szuperero = szuperero ;
+        this.vanEGyengesege = vanEGyengesege ;
+    }
+
+    public abstract boolean megmentiAVilagot();
 
 
     @Override
@@ -18,6 +21,28 @@ public abstract class Bosszuallo implements Szuperhos{
 
     @Override
     public double mekkoraAzEreje() {
-        return 0;
+        return this.szuperero;
+    }
+
+    public double getSzuprerero(){
+        return szuperero;
+    }
+
+    public void setSzuprerero(double szuperero){
+        this.szuperero = szuperero;
+    }
+
+    public boolean getVanEGyengesege() {
+        return vanEGyengesege;
+    }
+
+    public void setVanEGyengesege(boolean vanEGyengesege) {
+        this.vanEGyengesege = vanEGyengesege;
+    }
+
+    @Override
+    public String toString() {
+        return "Bosszúálló szuperereje: "+ szuperero +
+                ", Van gyengesége:  " + vanEGyengesege;
     }
 }
